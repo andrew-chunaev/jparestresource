b4079cdb12798e705f09ffebe34104dae1093a8c
Sweam <SSweam@yandex.ru>
2018-11-20 10:33:05 +0400

    Implemented create method

--- jparestresource/jparestresource-web/src/main/java/ru/ilb/jparestresource/web/DocumentsResourceImpl.java
+++ jparestresource/jparestresource-web/src/main/java/ru/ilb/jparestresource/web/DocumentsResourceImpl.java
@@ -69,7 +69,8 @@ public class DocumentsResourceImpl implements DocumentsResource {
 
     @Override
     public long create(Document document) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return documentRepository.save(documentMapper.createFromDto(document)).getId();
     }
 
     @Override
